29221d526b804950cf39160318ba9b159d6278fe
Brian Bycynski <high5bri@hotmail.com>
2018-08-06 12:07:30 -0400

    finished section 18

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenueNfo from './components/venueNfo';
+import Highlight from './components/Highlights';
+
 class App extends Component {
     render() {
         return (
-            <div style={{ height: '1500px', background: 'cornflowerblue' }}>
+            // <div style={{ height: '1500px', background: 'cornflowerblue' }}>
+            <div>
                 <Header />
                 <Featured />
+                <VenueNfo />
+                <Highlight />
             </div>
         );
     }
---
+++ src/components/Highlights/Description.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Magnam quos sunt assumenda quisquam eius maiores perferendis
+                    architecto consequuntur nostrum. Odit doloribus nostrum ex
+                    autem nihil adipisci natus maiores? Architecto similique
+                    eveniet, expedita quam amet ab sunt. Fuga nostrum odio, nemo
+                    voluptates totam cumque, qui quia hic, praesentium accusamus
+                    distinctio aliquid.
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
